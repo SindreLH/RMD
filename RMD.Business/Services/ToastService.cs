ad9566adcc21410e6cc207ecdcb22e723cd51a6e
Sindre <114007546+SindreLH@users.noreply.github.com>
2025-06-09 01:31:07 +0200

    - Refactoring of toast logic: Made a toast service, and made one single generic toast component. Injected service into parent components with relevant params.
    - Deleted old files: legacy folder and unused modals which was later replaced with toasts. Also deleted redundant toast-components.
    
    - Started commenting the project: ToastComponent and ToastService
    
    - Minor redesign on modals:
    Changed the background color and the main color on each modals bi-icon.

--- RMD.Business/Services/ToastService.cs
+++ RMD.Business/Services/ToastService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace RMD.Business.Services {
 
 	/// <summary>
-	/// ToastService is a singleton service which is using an event to notify a the toast component when it's supposed to be displayed.
+	/// ToastService is a singleton service which is using an event (OnShow) to notify a the toast component when it's supposed to be displayed.
 	/// This service can be thought of as a broadcaster. The toast component then listens for OnShow to be invoked, displaying the toast.
 	/// The enum is used by the component to decide which version of the toast is going to be displayed at given moments.
 	/// </summary>
