4b742bfe7213041ce7a6106eca4d90cd73a6ac88
Sindre <114007546+SindreLH@users.noreply.github.com>
2026-01-29 01:29:09 +0100

    RegEx Validation (MM:SS) added on Song Length Input on RegisterSong page:
    Switched HTML component from Input to InputText, updated DTO with RegEx annotation, Wrapped component in EdtiForm. Removed @OnClick from button - now handled by EditForm
    
    Same changes applied on EditSong Modal.

--- RMD.Data/Models/DTO/SongUpdateDto.cs
+++ RMD.Data/Models/DTO/SongUpdateDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,10 @@ namespace RMD.Data.Models.DTO
 	{
 		public int SongId { get; set; }
 		public string Title { get; set; } = string.Empty;
+
+		[Required]
+		[RegularExpression(@"^[0-5]?\d:[0-5]\d$",
+		ErrorMessage = "Length input must match format: MM:SS")]
 		public string Length { get; set; } = string.Empty;
 		public string Genre { get; set; } = string.Empty;
 		public bool ExtendedMix { get; set; }
